90f9daa87cc9b8f11eafa0e72997ec57d53a8d79
DanielNoerby <48019326+DanielNoerby@users.noreply.github.com>
2019-03-27 14:58:12 +0100

    forsøgt at indsætte tidslinje

---
+++ pages/Tidslinje.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!--Side titel  -->
+  <title>Café Frederiksberg</title>
+  <!--Standard stylesheet  -->
+  <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+  <!-- Skeleton stylesheet  -->
+  <link rel="stylesheet" type="text/css" href="../css/skeleton.css">
+  <!-- Normalize stylesheet  -->
+  <link rel="stylesheet" type="text/css" href="../css/normalize.css">
+  <!-- Includer Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Poppins" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <section id=timeline>
+  	<h1>A Flexbox Timeline</h1>
+  	<p class="leader">All cards must be the same height and width for space calculations on large screens.</p>
+  	<div class="demo-card-wrapper">
+  		<div class="demo-card demo-card--step1">
+  			<div class="head">
+  				<div class="number-box">
+  					<span>01</span>
+  				</div>
+  				<h2><span class="small">Subtitle</span> Technology</h2>
+  			</div>
+  			<div class="body">
+  				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+  				<img src="http://placehold.it/1000x500" alt="Graphic">
+  			</div>
+  		</div>
+
+  		<div class="demo-card demo-card--step2">
+  			<div class="head">
+  				<div class="number-box">
+  					<span>02</span>
+  				</div>
+  				<h2><span class="small">Subtitle</span> Confidence</h2>
+  			</div>
+  			<div class="body">
+  				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+  				<img src="http://placehold.it/1000x500" alt="Graphic">
+  			</div>
+  		</div>
+
+  		<div class="demo-card demo-card--step3">
+  			<div class="head">
+  				<div class="number-box">
+  					<span>03</span>
+  				</div>
+  				<h2><span class="small">Subtitle</span> Adaptation</h2>
+  			</div>
+  			<div class="body">
+  				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+  				<img src="http://placehold.it/1000x500" alt="Graphic">
+  			</div>
+  		</div>
+
+  		<div class="demo-card demo-card--step4">
+  			<div class="head">
+  				<div class="number-box">
+  					<span>04</span>
+  				</div>
+  				<h2><span class="small">Subtitle</span> Consistency</h2>
+  			</div>
+  			<div class="body">
+  				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+  				<img src="http://placehold.it/1000x500" alt="Graphic">
+  			</div>
+  		</div>
+
+  		<div class="demo-card demo-card--step5">
+  			<div class="head">
+  				<div class="number-box">
+  					<span>05</span>
+  				</div>
+  				<h2><span class="small">Subtitle</span> Conversion</h2>
+  			</div>
+  			<div class="body">
+  				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+  				<img src="http://placehold.it/1000x500" alt="Graphic">
+  			</div>
+  		</div>
+
+  	</div>
+  </section>
+
+
+
+
+<!--Indrag footer fra filen includes/footer.php-->
+<?php include '../includes/footer.php'; ?>
+
+</body>
+
+</html>
