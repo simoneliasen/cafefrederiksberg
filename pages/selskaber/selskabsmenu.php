ec1281bd53ebec2be50875c954812c92649b35fa
Victor <victor.s.lumholtz@gmail.com>
2019-03-27 16:24:00 +0100

    Menu style changes

--- pages/selskaber/selskabsmenu.php
+++ pages/selskaber/selskabsmenu.php
@@ -39,43 +39,22 @@
             <h2 class="header-centertext">Sammensæt din egen menu<h2>
                     <hr>
                     <h2 class="header-centertext"> Forretter </h2><br>
-                    <div class="container-retter">
-                        <div class="one columns"></div>
-                        <div class="seven columns">
-                            <h5> Klar suppe</h5>
-                            <p>Med boller og flütes</p>
-                            <h5>*Flødegeleret aspargessuppe</h5>
-                            <p>Med kødboller og flutes<p>
+        </div>
+        <div>
+            <div class="one columns"></div>
+            <div class="seven columns">
+                <div class="container-retter">
+                    <h5> Klar suppe</h5>
+                    <p>Med boller og flütes</p>
+                    <h5>*Flødegeleret aspargessuppe</h5>
+                    <p>Med kødboller og flutes<p>
+                </div>
 
-                        </div>
-                        <div class="four columns">
-                            <h5>- per couv. kr. 52,-</h5><br><br>
-                            <h5>- per couv. kr. 52,-</h5>
-                        </div>
-
-
-                    </div><br>
-                    <div class="wrapper indholdsoversigt">
-                        <!--Indhold centreret i wrapper-->
-                        <div class="container-katagori">
-                            <div class="container-retter">
-                                <div class="one columns"></div>
-                                <div class="seven columns">
-                                    <h5> grøn suppe</h5>
-                                    <p>Med boller og flütes</p>
-                                    <h5>*Flødegeleret aspargessuppe</h5>
-                                    <p>uden kødboller og flutes<p>
-
-                                </div>
-                                <div class="four columns">
-                                    <h5>- per couv. kr. 52,-</h5><br><br>
-                                    <h5>- per couv. kr. 52,-</h5>
-                                </div>
-
-
-                            </div>
-                        </div>
-                    </div>
+            </div>
+            <div class="four columns">
+                <h5>- per couv. kr. 52,-</h5><br><br>
+                <h5>- per couv. kr. 52,-</h5>
+            </div>
         </div>
     </div>
 
