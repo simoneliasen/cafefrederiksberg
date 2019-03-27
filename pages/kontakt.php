b7d7929ad769f351e0602293a42c342773470af8
DocLasse <48019299+DocLasse@users.noreply.github.com>
2019-03-27 13:55:26 +0100

    Container buttons

--- pages/kontakt.php
+++ pages/kontakt.php
@@ -37,6 +37,8 @@
   <div class="container indholdsoversigt">
     <h1> Her kommer der til at være en indholdsoversigt </h1>
     <!-- opretter række, som elementer let kan placeres i-->
+  </div>
+  <div class="container_buttons">
     <div class="row indholdsoversigt">
       <!--Opsætter kolonner der hver fylder 3 ud af 12 bredde (tjek hjemmeside skeletons hjemmeside)  -->
       <div class="three columns">
