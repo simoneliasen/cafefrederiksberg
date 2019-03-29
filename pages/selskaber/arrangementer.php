31b8bca8174a0ee7fe5e0a0383be3b338639e531
DanielNoerby <48019326+DanielNoerby@users.noreply.github.com>
2019-03-29 15:09:32 +0100

    rettet arrangementer

--- pages/selskaber/arrangementer.php
+++ pages/selskaber/arrangementer.php
@@ -32,15 +32,122 @@
 <div class="wrapper">
 
   <div class="container">
-      <h2> Konfirmation </h2>
+      <h2> Barnedåb </h2>
     <div class="row">
-      
+
       <div class="one-half column">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
 
       <div class="one-half column">
         <img src="../../img/butikfront.png" width="400rem" height="auto">
+      </div>
+
+      </div>
+
+</div>
+
+</div>
+
+
+<div class="wrapperarr">
+
+  <div class="container">
+      <h2> Konfirmation </h2>
+    <div class="row">
+
+      <div class="one-half column">
+        <img src="../../img/butikfront.png" width="400rem" height="auto">
+
+      </div>
+
+      <div class="one-half column">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+      </div>
+
+</div>
+
+</div>
+
+
+<div class="wrapper">
+
+  <div class="container">
+      <h2> Bryllup </h2>
+    <div class="row">
+
+      <div class="one-half column">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+      <div class="one-half column">
+        <img src="../../img/butikfront.png" width="400rem" height="auto">
+      </div>
+
+      </div>
+
+</div>
+
+</div>
+
+
+<div class="wrapperarr">
+
+  <div class="container">
+      <h2> Kaffebord </h2>
+    <div class="row">
+
+      <div class="one-half column">
+        <img src="../../img/butikfront.png" width="400rem" height="auto">
+
+      </div>
+
+      <div class="one-half column">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+      </div>
+
+</div>
+
+</div>
+
+<div class="wrapper">
+
+  <div class="container">
+      <h2> Julefrokost </h2>
+    <div class="row">
+
+      <div class="one-half column">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+      <div class="one-half column">
+        <img src="../../img/butikfront.png" width="400rem" height="auto">
+      </div>
+
+      </div>
+
+</div>
+
+</div>
+
+
+<div class="wrapperarr">
+
+  <div class="container">
+      <h2> Nytårsaften </h2>
+    <div class="row">
+
+      <div class="one-half column">
+        <img src="../../img/butikfront.png" width="400rem" height="auto">
+
+      </div>
+
+      <div class="one-half column">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
 
       </div>
