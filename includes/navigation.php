87dd2c5b7fdaecab3b80877b4df4b4dc67a75fc2
Simon Eliasen <simoneliasen@gmail.com>
2019-03-29 15:36:47 +0100

    Fjernede link fra header og satte links op i navigation

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,15 +1,17 @@
 <div class="navigationbar">
+<!--Container der centrere content i navigationsmenu  -->
   <nav class="container">
+    <!--Placeholder logo  -->
     <div class="logo">
     <h4 id="logo"> Café Frederiksberg</h4>
-</div>
-
+    </div>
+<!--Navigationsmenu m. relative path fra document root  -->
 <div class="navigationmenu">
-<p> RESTAURANT</p>
-<p> BAR</p>
-<p> EVENTS </p>
-<p> SELSKABER </p>
-<p> KONTAKT </p>
+<a href="/cafefrederiksberg/pages/restaurant.php">RESTAURANT</a>
+<a href="/cafefrederiksberg/pages/bar.php">BAR</a>
+<a href="/cafefrederiksberg/pages/events.php">EVENTS</a>
+<a href="/cafefrederiksberg/pages/selskaber.php">SELSKABER</a>
+<a href="/cafefrederiksberg/pages/kontakt.php">KONTAKT</a>
 </div>
   </nav>
 </div>
