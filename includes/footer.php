9c3d518cae4a64096385ebbe831a7a118d7d74ba
Mona Lisa <simoneliasen@gmail.comgit>
2019-03-31 14:03:02 +0200

    Template opsætning for footer (inline css)

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,54 @@
   <div class="fullWidthFooter">
 
     <footer class="container">
+<div class="row">
 
-    <center>
-        <p> Her kommer der til at være en fucking footer! </p>
-</center>
-    </footer>
+  <!--Kontakt column  -->
+  <div class="four columns kontaktos">
+    <ul>Kontakt os </ul>
+    <li>Addresse</li>
+    <li>Telefon</li>
+    <li>E-mail</li>
+    <li>Instagram</li>
+    <li>facebook</li>
   </div>
+
+    <!--åbningstider column  -->
+  <div class="four columns åbningstider">
+    <ul>Åbningstider</ul>
+    <li>Mandag</li>
+    <li>Tirsdag</li>
+    <li>Onsdag</li>
+    <li>Torsdag</li>
+    <li>Fredag</li>
+    <li>Lørdag</li>
+    <li>Søndag</li>
+
+    <ul>Køkken</ul>
+    <li> Hver dag: </li>
+  </div>
+
+  <!--links column  -->
+  <div class="four columns links">
+    <ul>Links</ul>
+    <li>Feriehus Florida</li>
+    <li>Sport</li>
+    <li>Kontrol Rapport</li>
+  </div>
+
+</div>
+    </footer>
+
+
+<style>
+
+
+
+.four.columns.kontaktos ul,.four.columns.kontaktos li, .four.columns.åbningstider ul,.four.columns.åbningstider li,.four.columns.links ul,.four.columns.links li {
+  text-align: center;
+  list-style-type: none;
+}
+
+
+
+</style>
