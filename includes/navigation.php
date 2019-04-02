fa2f31f3af5bf61b9ea622400fb306d7a1591022
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-02 14:58:43 +0200

    Gg, tilføjede jquery for interaktiv header v. scroll

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,3 +1,11 @@
+
+<!--Henter Jquery Script via CDN  -->
+<script
+  src="https://code.jquery.com/jquery-1.12.4.js"
+  integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+  crossorigin="anonymous"></script>
+
+<!--main navigationsbar  -->
 <div class="navigationbar">
 
 <!--Container der centrere content i navigationsmenu  -->
@@ -8,7 +16,7 @@
     <a href="/cafefrederiksberg/index.php"><img src="/cafefrederiksberg/img/logo.svg" id="logo" width="150rem" height="auto"></a>
     </div>
 
-<!--Navigationsmenut(root skal  hedde caffrederiksberg, før den virker (ændres når den tages online)) -->
+<!--Navigationsmenu(root skal  hedde caffrederiksberg, før den virker (ændres når den tages online)) -->
 <div class="navigationmenu">
 <a href="/cafefrederiksberg/pages/restaurant.php">Restaurant</a>
 <a href="/cafefrederiksberg/pages/bar.php">Bar</a>
@@ -22,11 +30,12 @@
   <a href="/cafefrederiksberg/pages/selskaber/arrangementer.php">Arrangementer</a>
   <a href="/cafefrederiksberg/pages/selskaber/selskabsmenu.php">Selskabsmenu</a>
   </div>
-
 </div>
+
+<!--Rest menu (kontakter)  -->
 <a href="/cafefrederiksberg/pages/kontakt.php">Kontakt</a>
 </div>
-
+<!--Line to sepperate content  -->
 <hr id="headerline">
 
 </nav>
@@ -40,6 +49,7 @@
   top: 0; /* Position the navbar at the top of the page */
   width: 100%;
   z-index: 1;  /*puts header in front of content*/
+  background: transparent;
   }
 .logo {
   display: inline;
@@ -70,7 +80,6 @@
 #headerline {
   margin: 0;
   border-color: white;
-
 }
 
 /* CSS for dropdown funktion */
@@ -78,7 +87,6 @@
   position: relative;
   display: inline-block;
 }
-
 .dropdown-content {
   display: none;
   position: absolute;
@@ -86,9 +94,15 @@
   padding: 12px 16px;
   z-index: 1;
 }
-
 .dropdown:hover .dropdown-content {
   display: block;
 }
-
 </style>
+
+<!-- Jquery Script that changes the color of the header on scroll  -->
+<script>
+var $nav = $('.navigationbar');
+$(document).scroll(function() {
+    $nav.css({background: $(this).scrollTop() > 100? "black":"transparent"});
+});
+</script>
