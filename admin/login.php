cf13fef20aa97b409f2f9050704dc5d9bb0edc9e
Simon Eliasen <simoneliasen@gmail.com>
2019-04-03 10:43:56 +0200

    Uploadede CSS til stylesheet + Startede på login form

---
+++ admin/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<!--Background farve  -->
+<div class="loginbackground">
+  <div class="loginbacktoindex">
+    <p>-- Tilbage til Forsiden </p>
+  <!--Logo  -->
+<div class="loginlogo">
+  <a href="/cafefrederiksberg/index.php"><img src="/cafefrederiksberg/img/logo.svg" id="logo" width="150rem" height="auto"></a>
+</div>
+
+</div>
+</div>
+
+<style>
+
+
+.loginbackground {
+  width: 100%
+  height: 100%;
+  background-color: #1E1D20;
+}
+
+
+.loginbacktoindex {
+  float: left;
+}
+
+.loginlogo {
+  text-align: center;
+}
+
+</style>
+
+
+</body>
+</html>
