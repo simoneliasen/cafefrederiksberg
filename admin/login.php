136a4963c21d5a8386dfd6184a246104125fe74b
Simon Eliasen <simoneliasen@gmail.com>
2019-04-03 11:01:36 +0200

    Login opdatering

--- admin/login.php
+++ admin/login.php
@@ -1,34 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+
+  <!--Side titel  -->
+  <title>Café Frederiksberg</title>
+  <!--Standard stylesheet  -->
+  <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+  <!-- Skeleton stylesheet  -->
+  <link rel="stylesheet" type="text/css" href="../css/skeleton.css">
+  <!-- Normalize stylesheet  -->
+  <link rel="stylesheet" type="text/css" href="../css/normalize.css">
+  <!-- Includer Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
 </head>
+
 <body>
 
 <!--Background farve  -->
 <div class="loginbackground">
+
+
   <div class="loginbacktoindex">
     <p>-- Tilbage til Forsiden </p>
+</div>
+
+
+    <div class="loginform">
+
+
   <!--Logo  -->
 <div class="loginlogo">
-  <a href="/cafefrederiksberg/index.php"><img src="/cafefrederiksberg/img/logo.svg" id="logo" width="150rem" height="auto"></a>
+  <a href="/cafefrederiksberg/index.php"><img src="/cafefrederiksberg/img/logo.svg" id="logo" width="500rem" height="auto"></a>
 </div>
+
+<form action="action_page.php">
+  <div class="imgcontainer">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+
+
+
+
+
 
 </div>
 </div>
+
+
 
 <style>
 
 
-.loginbackground {
-  width: 100%
-  height: 100%;
-  background-color: #1E1D20;
-}
 
+
+.loginform {
+  margin-top: 10rem;
+}
 
 .loginbacktoindex {
   float: left;
