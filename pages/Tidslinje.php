618d3e1a79dfb0abd6bf9ed7376a425db7a6b347
bareFink <frederikhojriis@gmail.com>
2019-04-04 14:22:00 +0200

    header og tidslinje

--- pages/Tidslinje.php
+++ pages/Tidslinje.php
@@ -34,15 +34,15 @@
   <h2 style="text-align: center; margin: 4rem 0 2rem 0">Tidslinje</h2>
 
 
-
+<div class="bg_dark">
   <div class="container">
 
     <div class="middleline"></div>
     <div class="row">
 
       <div class="five columns">
-        <div class="timeline_line timeline_line_first timeline_line_left"></div>
-        <div class="timeline_item timeline_item_first">
+        <div class="timeline_line timeline_line_left"></div>
+        <div class="timeline_item">
           <h2 style="margin-bottom: 1rem;">1900</h2>
           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
           <div class="timeline_img"></div>
@@ -88,40 +88,9 @@
         </div>
       </div>
     </div>
+  </div>
 
-
-              <!-- <div class="line_item2">
-                <h2>1909</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                <img src="../img/Butikfront.png" width="400rem" height="auto">
-              </div>
-
-              <div class="line_item3">
-                <h2>1930</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                <img src="../img/Butikfront.png" width="400rem" height="auto">
-              </div>
-
-              <div class="line_item4">
-                <h2>1950</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                <img src="../img/Butikfront.png" width="400rem" height="auto">
-              </div>
-
-              <div class="line_item5">
-                <h2>1973</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                <img src="../img/Butikfront.png" width="400rem" height="auto">
-              </div>
-
-              <div class="line_item6">
-                <h2>2006</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                <img src="../img/Butikfront.png" width="400rem" height="auto">
-              </div> -->
-
-      </div>
-    </div>
+</div>
 
 
 
