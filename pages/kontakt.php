c9cb8d9eacc7c03da72cfa2eb5407bf860c83cab
DocLasse <48019299+DocLasse@users.noreply.github.com>
2019-04-04 14:30:53 +0200

    Genskabelse af kontaktside

--- pages/kontakt.php
+++ pages/kontakt.php
@@ -31,9 +31,8 @@
 </div>
 
 
-  <!--Eksempel m. 3-delt container og overskrift  -->
-  <div class="wrapper kontakt_info">
-    <!--Indhold centreret i wrapper-->
+<div class="wrapper kontakt">
+  <div class="bg_dark">
     <div class="container kontakt_info">
       <div class="kontakt_info_buttons">
         <div class="row kontakt_info">
@@ -42,63 +41,120 @@
             <div class="Bestil_online"><h2>Bestil online:</h2></div>
           </div>
           <div class="two columns">
-            <a class="button button-primary" href="#">Bord</a>
+            <a class="button button-primary bestil" href="#">Bord</a>
           </div>
           <div class="two columns">
-            <a class="button button-primary" href="#">Biletter</a>
+            <a class="button button-primary bestil" href="#">Biletter</a>
           </div>
           <div class="two columns">
-            <a class="button button-primary" href="#">Take-away</a>
+            <a class="button button-primary bestil" href="#">Take-away</a>
           </div>
         </div>
       </div>
     </div>
   </div>
-    <!-- Kontakt info -->
-    <div class="container opening_info">
-      <div class="opening_info">
-        <div class="row opening_info">
-          <div class="one-third">
-            <div class="opening_float_left">
-              <h2>Åbningstid:</h2>
-                <ul class="opening_times">
-                  <li><strong>Mandag:</strong>  kl. 10.00-00.00</li>
-                  <li><strong>Tirsdag:</strong> kl. 10.00-00.00</li>
-                  <li><strong>Onsdag:</strong>  Kl. 10.00-00.00</li>
-                  <li><strong>Torsdag:</strong> Kl. 10.00-03.00</li>
-                  <li><strong>Fredag:</strong>  Kl. 10.00-03.00</li>
-                  <li><strong>Lørdag:</strong>  Kl. 08.00-03.00</li>
-                  <li><strong>Søndag:</strong>  Kl. 08.00-00.00</li>
-                </ul>
-              <h2>Køkken:</h2>
-              <ul class="opening_times">
-                <li><strong>Hver dag:</strong>  Kl. 12.00-14.00 & 17.00-20.00</li>
-              </div>
+
+  <!-- Kontakt info -->
+  <div class="container opening_times">
+    <div class="row">
+        <div class="six columns">
+          <h2>Åbningstider:</h2>
+
+
+            <div class="opening_day">
+              Mandag
             </div>
-            <div class="one-third">
+            <div class="opening_times">
+              Kl. 10.00-00.00
             </div>
-            <div class="one-third">
-              <div class="kontaktinfo_float_right">
-                <h2>Find os her</h2>
-                  <ul class="opening_times">
-                    <li><strong>Hadsundvej 1B</strong></li>
-                    <li><strong>9000 Aalborg</strong></li>
-                    <li><strong>Danmark</strong></li>
-                    <li><strong>(+45) 98 12 03 83</strong></li>
-                    <li><strong>cafefrederiksberg@gmail.com</strong></li>
-                  </ul>
-                <h2>Vedrørende fest</h2>
-                <ul class="opening_times">
-                  <li><strong>(+45) 22 42 43 00</strong></li>
-                  <li><strong>Frederiksbergselskaber@gmail.com</strong></li>
-                </ul>
-              </div>
+
+
+          <div class="buffet_price">
+            <div class="buffet_price_info">
+              Tirsdag
             </div>
+            <div class="buffet_price_price">
+              Kl. 10.00-00.00
+            </div>
+          </div>
+
+          <div class="buffet_price">
+            <div class="buffet_price_info">
+              Onsdag
+            </div>
+            <div class="buffet_price_price">
+              Kl. 10.00-00.00
+            </div>
+          </div>
+
+          <div class="buffet_price">
+            <div class="buffet_price_info">
+              Torsdag
+            </div>
+            <div class="buffet_price_price">
+              Kl. 10.00-03.00
+            </div>
+          </div>
+
+          <div class="buffet_price">
+            <div class="buffet_price_info">
+              Fredag
+            </div>
+            <div class="buffet_price_price">
+              Kl. 10.00-03.00
+            </div>
+          </div>
+
+          <div class="buffet_price">
+            <div class="buffet_price_info">
+              Lørdag
+            </div>
+            <div class="buffet_price_price">
+              Kl. 08.00-03.00
+            </div>
+          </div>
+
+          <div class="buffet_price">
+            <div class="buffet_price_info">
+              Søndag
+            </div>
+            <div class="buffet_price_price">
+              Kl. 08.00-00.00
+            </div>
+          </div>
+
         </div>
+
+
+      <div class="six columns">
+        <div class="buffet_placeholder_img_right"></div>
       </div>
+
     </div>
+  </div>
+</div>
+<!--Google maps placeholder  -->
+<center>
+  <div class="mapouter">
+    <div class="gmap_canvas"><iframe width="1920" height="575" id="gmap_canvas" src="https://maps.google.com/maps?q=Caf%C3%A9%20Frederiksberg%20Aalborg&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0"
+        marginwidth="0"></iframe><a href="https://www.emojilib.com"></a></div>
+    <style>
+      .mapouter {
+        position: relative;
+        text-align: right;
+        height: 575px;
+        width: 1920px;
+      }
 
-
+      .gmap_canvas {
+        overflow: hidden;
+        background: none !important;
+        height: 575px;
+        width: 1920px;
+      }
+    </style>
+  </div>
+</center>
 
 
 <!--Indrag footer fra filen includes/footer.php-->
