4d108465b2e1d77083b9e98fa44abe6ff41d5f43
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-04 19:04:48 +0200

    Så skulle footeren gerne være i huuzs

--- includes/footer.php
+++ includes/footer.php
@@ -1,32 +1,127 @@
-  <div class="fullWidthFooter">
 
-    <footer class="container">
-<div class="row">
+
+
+<div class="fullWidthFooter">
+ <footer class="container">
+  <div class="row">
 
   <!--Kontakt column  -->
-  <div class="four columns kontaktos">
-
+<div class="four columns kontaktos">
     <ul>Kontakt os </ul>
 
-<div class="kontaktleft">
-<li><strong>Adresse</strong></li><br><br><br><br>
-<li><strong>Telefon:</strong></li><br>
-<li><strong>E-mail </strong></li><br><br>
-  <li><img src="/cafefrederiksberg/img/instagram.png">Instagram</li><br>
-  <li><img src="/cafefrederiksberg/img/facebook.png">facebook</li><br>
-</div>
+
+<!--Wrapper  -->
+    <div class="row adresse-wrapper">
+<!--Left  -->
+      <div class="adresseleft">
+        <p><strong>Adresse:</strong></p>
+      </div>
+<!--Right  -->
+      <div class="adresseright">
+        <p>Hadsundvej 1B </p>
+        <p>9000 Aalborg </p>
+        <p>Danmark </p>
+      </div>
+    </div>
 
 
-<div class="kontaktright">
-<li>Hadsundvej 1B</li><br>
-  <li>9000 Aalborg</li><br>
-  <li>Danmark</li><br>
- <li>(+45) 98 12 03 83</li><br>
-  <li>cafefrederiksberg@gmail.com</li><br>
+<!--Wrapper  -->
+    <div class="row telefon-wrapper">
+<!--Left  -->
+      <div class="telefonleft">
+      <p><strong>Telefon:</strong></p>
+      <p class="tilselskaber">(Til selskaber)</p>
+      </div>
+<!--Right  -->
+      <div class="telefonright">
+        <p>(+45) 98 12 03 83 </p>
+        <p class="tilselskaber">(+45) 98 12 03 83 </p>
+      </div>
+    </div>
 
 
-</div>
+    <!--Wrapper  -->
+        <div class="row e-mail-wrapper">
+    <!--Left  -->
+          <div class="e-mailleft">
+            <p><strong>E-mail:</strong></p>
+            <p class="tilselskaber">(Til selskaber)</p>
 
+          </div>
+    <!--Right  -->
+          <div class="e-mailright">
+            <p>cafefrederiksberg@gmail.com </p>
+            <p class="tilselskaber">frederiksbergselskaber@gmail.com</p>
+          </div>
+        </div>
+
+
+
+<style>
+
+
+
+
+/*Adresse  */
+.row.adresse-wrapper {
+  display: block;
+}
+.adresseleft {
+  display: inline;
+  float: left;
+}
+.adresseright {
+    display: inline;
+      float: right;
+        text-align: right;
+}
+
+
+
+/*Telefon  */
+.row.telefon-wrapper {
+  display: block
+margin-top: 1.3rem;
+}
+.telefonleft {
+  display: inline;
+  float: left;
+}
+.telefonright {
+  display: inline;
+  float: right;
+  text-align: right;
+}
+
+/*E-mail */
+.row.e-mail-wrapper {
+  display: block;
+margin-top: 1rem;
+}
+.e-mailleft {
+  display: inline;
+    float: left;
+}
+.e-mailright {
+    display: inline;
+  float: right;
+  text-align: right;
+}
+
+.tilselskaber {
+  font-size: 0.8rem;
+  font-style: italic;
+}
+</style>
+
+
+
+
+
+  <!--
+
+    <img src="/cafefrederiksberg/img/instagram.png">Instagram</li>
+    <img src="/cafefrederiksberg/img/facebook.png">facebook</li>> -->
 
 
   </div>
@@ -41,21 +136,22 @@
     <li><strong>Fredag:</strong>  Kl. 10.00-03.00</li>
     <li><strong>Lørdag:</strong>  Kl. 08.00-03.00</li>
     <li><strong>Søndag:</strong>  Kl. 08.00-00.00</li>
+
+      <!--Køkkens åbningstider  -->
+
   <!--Køkkens åbningstider  -->
-    <ul>Køkken</ul>
-    <li><strong>Hver dag:</strong>  Kl. 12.00-14.00 & 17.00-20.00</li>
+
   </div>
 
   <!--Links  -->
   <div class="four columns links">
-    <ul>Links</ul>
-    <li><a href="http://www.orlando-florida.dk/">Feriehus Florida</a></li>
+    <ul>Køkken</ul>
+<li> Kl. 12.00-14.00 & 17.00-20.00</li>
 
-    <li><a href="https://www.findsmiley.dk/25727">Kontrol Rapport</a></li>
-<!--Vedrørende fest/selskaber  -->
-    <ul> Vedrørende fest</ul>
-    <li><strong>(+45) 22 42 43 00</strong></li>
-    <li><strong>Frederiksbergselskaber@gmail.com</strong></li>
+<ul> Links </ul>
+<li><a href="http://www.orlando-florida.dk/">Feriehus Florida</a></li>
+<li><a href="https://www.findsmiley.dk/25727">Kontrol Rapport</a></li>
+
   </div>
 
 </div>
@@ -68,28 +164,13 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <style>
 .four.columns.åbningstider ul,.four.columns.links ul ,.four.columns.kontaktos ul {
   font-size: 30px;
   font-family: "Poppins";
   font-weight: bold;
-  margin-top: 6rem;
+  margin-top: 1.5rem;
+
 }
 
 .four.columns.kontaktos ul,.four.columns.kontaktos li, .four.columns.åbningstider ul,.four.columns.åbningstider li,.four.columns.links ul,.four.columns.links li {
@@ -97,28 +178,6 @@
   list-style-type: none;
 }
 
-
-
-
-
-
 /* Placement of footer material additional */
-.kontaktleft {
-  display: inline;
-  width: 50%;
-  float: left;
 
-}
-
-.kontaktleft > li {
-  display: inline-block;
-}
-.kontaktright > li {
-  display: inline-block;
-}
-.kontaktright{
-  display: inline;
-  width: 50%;
-    float: right;
-}
 </style>
