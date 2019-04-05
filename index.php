cc6305851c1ea5420282e8551cd396f32f43cda6
Simon Eliasen <simoneliasen@gmail.com>
2019-04-05 10:42:50 +0200

    Lavede knapper hvide på forsiden

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <!-- Link til katalog  -->
-            <a class="button" href="#">Se vores Magsin <u>her</u></a>
+            <a class="button magasin" href="#">Se vores Magasin <u>her</u></a>
           </div>
         </div>
 
