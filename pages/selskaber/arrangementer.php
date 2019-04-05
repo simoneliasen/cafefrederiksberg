f3744e9d140923330fbebdd5effb8f9a51c8e2d4
DanielNoerby <48019326+DanielNoerby@users.noreply.github.com>
2019-04-05 11:07:36 +0200

    selskaber css

--- pages/selskaber/arrangementer.php
+++ pages/selskaber/arrangementer.php
@@ -30,8 +30,13 @@
 
 
 <div class="wrapper">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <div class="bg_dark">
+  <div class="container info">
+    <h2 class="arrheader">Arrangementer på Cafe Frederiksberg</h2>
 
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+</div>
   <div class="container arrangementer">
     <div class="arrangementer_wrapper">
       <h2 style="margin-bottom: 1rem;"> Barnedåb </h2>
