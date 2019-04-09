40aa874f8017cdb9d14fb42b9e42e242969ac7d7
bareFink <frederikhojriis@gmail.com>
2019-04-09 20:24:47 +0200

    tilføjede mere til events

--- pages/events.php
+++ pages/events.php
@@ -61,23 +61,46 @@
 
 <div class="container">
   <div class="events_overskrift">
-    <h2>Frede Live</h2>
-    <h6>Kommende Events</h6>
+<!--Overskrift logo  -->
+    <div class="events_overskrift_leftalign">
+      <img src="/cafefrederiksberg/img/fredelivelogo.svg" id="eventlogo" width="60rem" height="auto">
+    </div>
+<!-- Overskrift tekst  -->
+    <div class="events_overskrift_rightalign">
+      <h2>Frede Live</h2>
+      <h6>Kommende Events</h6>
+    </div>
   </div>
 <div class='sk-fb-event' data-embed-id='22281'></div><script src='https://www.sociablekit.com/app/embed/facebook-events/widget.js'></script>
 </div>
 
 <div class="bg_dark">
-  <div class="container" style="padding-bottom: 2rem;">
-    <div class="events_overskrift">
-      <h2>Følg os på Instagram</h2>
-      <h6>Nyheder og billeder fra Frede</h6>
+  <div class="container">
+    <h2>Klub Frede</h2>
+    <h6>Bliv medlem i Klub Frede og spar penge</h6>
+    <div class="row klub_frede">
+      <div class="one-half column">
+
+      </div>
+
+      <div class="one-half column">
+        <div class="events_overskrift_leftalign">
+          <img src="/cafefrederiksberg/img/fredelivelogo.svg" id="eventlogo" width="auto" height="100px">
+        </div>
+      </div>
     </div>
-    <script src="https://snapwidget.com/js/snapwidget.js"></script>
-    <iframe src="https://snapwidget.com/embed/678509" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%; "></iframe>
-    <div style="font:10px/14px 'Roboto','Helvetica Neue',Arial,Helvetica,sans-serif;font-weight:400;width:100%;text-align:right"><a href="https://snapwidget.com" style="color:#777;text-decoration:none;">SnapWidget · Instagram Widget</a></div>
   </div>
-</div class="bg dark">
+</div>
+
+<div class="container" style="padding-bottom: 2rem;">
+  <div class="events_overskrift">
+    <h2>Følg os på Instagram</h2>
+    <h6>Nyheder og billeder fra Frede</h6>
+  </div>
+  <script src="https://snapwidget.com/js/snapwidget.js"></script>
+  <iframe src="https://snapwidget.com/embed/678509" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%; "></iframe>
+  <div style="font:10px/14px 'Roboto','Helvetica Neue',Arial,Helvetica,sans-serif;font-weight:400;width:100%;text-align:right"><a href="https://snapwidget.com" style="color:#777;text-decoration:none;">SnapWidget · Instagram Widget</a></div>
+</div>
 
 <?php include '../includes/footer.php'; ?>
 
