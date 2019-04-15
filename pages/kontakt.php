6dcd72aac5e0757c3e55f13ae6b36e80eb52d58d
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-15 21:06:58 +0200

    Tilføjede titel til iframes

--- pages/kontakt.php
+++ pages/kontakt.php
@@ -214,9 +214,9 @@
 </div>
 
 <!--Google maps  -->
-  <div class="mapouter">
-    <div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=Caf%C3%A9%20Frederiksberg%20Aalborg&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0"
-        marginwidth="0"></iframe><a href="https://www.emojilib.com"></a></div>
+<div class="mapouter">
+  <div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas" title="google maps location" src="https://maps.google.com/maps?q=Caf%C3%A9%20Frederiksberg%20Aalborg&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0"
+      marginwidth="0"></iframe><a href="https://www.emojilib.com"></a></div>
 </div>
 
 
