fb9ac14fc591a9bec45f88f95c2228af22868782
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-16 12:24:17 +0200

    Gjorde rent i announcement header

--- includes/announcement.php
+++ includes/announcement.php
@@ -3,16 +3,14 @@
   integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
   crossorigin="anonymous"></script>
 
-  <!-- Disable .promotionheader (headeren har top:0 istedet for top: 35 som ved announcement) ved fravalg af announcementbar -->
   <!--Sætter margin: 0 hvis announcementbar ikke er present og 20 hvis den er (så de ikke overlapper)  -->
-  <div class="promotionheader">
   <script>
   var $nav = $('.navigationbar');
   $(document).scroll(function() {
       $nav.css({top: $(this).scrollTop() > 20? 0:35});
   });
   </script>
-  </div>
+
 
 
 <div class="announcement">
