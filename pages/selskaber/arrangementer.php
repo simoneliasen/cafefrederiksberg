a3f16398c2f11ac7ed39f84e1d84e1979967e387
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-16 12:22:38 +0200

    Fjernede margin på arrangementer

--- pages/selskaber/arrangementer.php
+++ pages/selskaber/arrangementer.php
@@ -33,7 +33,7 @@
 
 <div class="wrapper">
   <div class="bg_dark">
-  <div class="container info">
+  <div class="container">
     <h2 class="arrheader">Arrangementer på Cafe Frederiksberg</h2>
 
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
