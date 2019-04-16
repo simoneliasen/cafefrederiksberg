e4d8079d76a2407cdc9f6c17ca4e2b91e623e485
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-16 13:35:25 +0200

    Tilgå login.php via "cafefrederiksberg/admin" istedet

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+
+<!--Redirecter til login.php  -->
+<?php
+header("Location: login.php");
+die();
+ ?>
+
+
+ <!--Gør at Café Frederiksberg bare kan skrive
+caféfrederiksberg.dk/ADMIN
+istedet for caféfrederiksberg.dk/admin/login.php  -->
