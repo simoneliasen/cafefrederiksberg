2f4c2a786cd9d27fc7fe4c0af779740735023ace
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-16 16:14:56 +0200

    Rettede en lille uupsie med filsti

--- pages/selskaber/selskabsmenu.php
+++ pages/selskaber/selskabsmenu.php
@@ -23,11 +23,11 @@
 
 <body>
   <!--Tilføjer mulighed for announcementbar  -->
-  <?php include '../includes/announcement.php'; ?>
+  <?php include '../../includes/announcement.php'; ?>
   <!--Inddrager navigationsbar fra "includes/navigation.php"-->
-  <?php include '../includes/navigation.php'; ?>
+  <?php include '../../includes/navigation.php'; ?>
   <!--Slider (behøver ikke container, da den skal have 100% bredde) -->
-  <?php include '../includes/header.php'; ?>
+  <?php include '../../includes/header.php'; ?>
 
   <div class="container">
     <div class="row selskab_present">
