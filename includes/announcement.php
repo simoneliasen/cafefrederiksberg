39f6657d55a8278d29cf60ab9bd5563249a576a7
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-17 21:12:27 +0200

    Så er der sgu tidsindstillet announcementbar

--- includes/announcement.php
+++ includes/announcement.php
@@ -1,4 +1,10 @@
-<script
+
+
+  <div class="announcementwrapper">
+
+
+
+  <script
   src="https://code.jquery.com/jquery-1.12.4.js"
   integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
   crossorigin="anonymous"></script>
@@ -13,6 +19,50 @@
 
 
 
-<div class="announcement">
-  <p style="text-align: center; font-size: 1.1rem; padding: 6px 0; margin:0;">STEGT FLÆSK OG FISSE 2050 kr!</p>
+    <div class="announcement">
+    <p style="text-align: center; font-size: 1.1rem; padding: 6px 0; margin:0;">
+
+
+    <?php
+    //Opretter forbindelse via config.php, og tjekker om du er logget ind via session.php
+    $connection = mysqli_connect('localhost','root','','admin');
+
+    if(!$connection){
+      die("Cannot connect to the database" . mysqli_connect_error());
+    }
+
+    $query ="SELECT * FROM announcements";
+    $results = mysqli_query($connection,$query);
+    if(!$results){
+      die("could not query the database" .mysqli_error());
+    }
+
+    // Announcement besked fra database announcement besked (indsat fra adminpanel)
+    $row = mysqli_fetch_row($results);
+    echo  $row[1];
+    ?>
+
+
+</p>
+</div>
+
+
+
+<?php
+// Tager datoen fra databasen og sammenligner med dags dato
+ $date = new DateTime($row[2]);
+ $now = new DateTime();
+
+ if($date < $now) {
+   echo '<style>
+   .announcementwrapper {
+     display: none;
+   }
+
+   </style>';
+ }
+?>
+
+
+
 </div>
