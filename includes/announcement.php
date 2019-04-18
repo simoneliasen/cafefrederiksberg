832f6192f78231a6b1e410d3af4b18b0a3311af2
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-18 15:03:39 +0200

    Fixede unreliable announcementbar + header

--- includes/announcement.php
+++ includes/announcement.php
@@ -1,68 +1,54 @@
-
-
-  <div class="announcementwrapper">
-
-
-
-  <script
-  src="https://code.jquery.com/jquery-1.12.4.js"
-  integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
-  crossorigin="anonymous"></script>
-
-  <!--Sætter margin: 0 hvis announcementbar ikke er present og 20 hvis den er (så de ikke overlapper)  -->
-  <script>
-  var $nav = $('.navigationbar');
-  $(document).scroll(function() {
-      $nav.css({top: $(this).scrollTop() > 20? 0:35});
-  });
-  </script>
-
-
-
-    <div class="announcement">
-    <p style="text-align: center; font-size: 1.1rem; padding: 6px 0; margin:0;">
-
-
-    <?php
-    //Opretter forbindelse via config.php, og tjekker om du er logget ind via session.php
-    $connection = mysqli_connect('localhost','root','','admin');
-
-    if(!$connection){
-      die("Cannot connect to the database" . mysqli_connect_error());
-    }
-
-    $query ="SELECT * FROM announcements";
-    $results = mysqli_query($connection,$query);
-    if(!$results){
-      die("could not query the database" .mysqli_error());
-    }
-
-    // Announcement besked fra database announcement besked (indsat fra adminpanel)
-    $row = mysqli_fetch_row($results);
-    echo  $row[1];
-    ?>
-
-
-</p>
-</div>
-
-
+<!-- Loads Jquery Library  -->
+<script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=" crossorigin="anonymous"></script>
 
 <?php
-// Tager datoen fra databasen og sammenligner med dags dato
- $date = new DateTime($row[2]);
- $now = new DateTime();
+//Opretter forbindelse via config.php, og tjekker om du er logget ind via session.php
+$connection = mysqli_connect('localhost','root','','admin');
+if(!$connection){
+  die("Cannot connect to the database" . mysqli_connect_error());
+}
 
- if($date < $now) {
-   echo '<style>
-   .announcementwrapper {
-     display: none;
-   }
+$query ="SELECT * FROM announcements";
+$results = mysqli_query($connection,$query);
+if(!$results){
+  die("could not query the database" .mysqli_error());
+}
 
-   </style>';
- }
+// Konventere queery ind til array
+$row = mysqli_fetch_row($results);
+
+ $date = new DateTime($row[2]); //Indtastede dato i database
+ $now = new DateTime(); //dags dato
+
+
+ if($date > $now) { //hvis dags dato er mindre end afsatte tid til announcementbar
+
+   ?>
+   <div class="announcementwrapper">
+     <script>
+       var $nav = $('.navigationbar');
+       $(document).scroll(function() {
+         $nav.css({top: $(this).scrollTop() > 35? 0:35});
+       });
+     </script>
+     <div class="announcement">
+       <p style="text-align: center; font-size: 1.1rem; padding: 6px 0; margin:0;"> <?php echo  $row[1]; ?> </p>
+     </div>
+   </div>
+  <?php
+
+} else { //hvis dags dato er højere end afsatte tid til announcementbar
+
 ?>
+<script>
+  var $nav = $('.navigationbar');
+  $(document).scroll(function() {
+    $nav.css({top: $(this).scrollTop() > 0? 0:0});
+  });
+</script>
+<?php
 
+};
 
-
-</div>
+mysqli_close($connection);
+?>
