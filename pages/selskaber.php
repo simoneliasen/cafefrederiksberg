80c7621220a67f65388e76366e6363c24f754808
Mona Lisa <simoneliasen@gmail.comgit>
2019-04-30 20:01:17 +0200

    Tekst til selskaber

--- pages/selskaber.php
+++ pages/selskaber.php
@@ -7,7 +7,6 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="description" content="Café Frederiksberg sørger både for en god buffet, en skræddersyet menu til dit selskab, udlejning af lokaler, eller blot nogle hyggelige rammer til dit selskab her i Aalborg"/>
   <link rel="icon" type="image/png" href="img/favicon.ico">
-
   <!--Side titel  -->
   <title>Café Frederiksberg</title>
   <!--Standard stylesheet  -->
@@ -18,7 +17,6 @@
   <link rel="stylesheet" type="text/css" href="../css/normalize.css">
   <!-- Includer Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Poppins" rel="stylesheet">
-
 </head>
 
 <body>
@@ -30,23 +28,20 @@
   <?php include '../includes/header.php'; ?>
 
 <div class="selskaber_wrapper">
-  <div class="container arrangementer">
+    <div class="row">
+      <div class="container arrangementer">
 
-
-      <div class="row">
         <div class="one-half column">
           <img src="img/buffet_placeholder.png.jpg" width="370rem" height="auto">
         </div>
         <div class="one-half column">
           <span class="selskab_text_title"> Vi byder på lokaler til selskaber, arrangementer og lækre buffeter!</span><br>
-          <p style="width: 400px;">Lorem ipsum dolor sit amet, consectetur
-             adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-             exercitation ullamco laboris nisi ut aliquip ex ea commodo
-             consequat.Duis aute irure dolor in reprehenderit in voluptate
-             velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-             occaecat cupidatat non proident, sunt in culpa qui officia
-             deserunt mollit anim id est laborum.</p>
+          <p style="width: 400px; margin-top: 1rem;">
+            Café Frederiksberg står til rådighed ved alle lejlighder, om det er formelle omstændigheder.
+            Eller det er en samling af gamle bekendte, så skal vi nok levere om det så enten drejer sig om mad, drikkelse eller blot nogle lækre omgivelser.
+
+            Et udsnit af arrangementerne vi har stået får før, kan du se under vores side "arrangementer",
+            derudover tilbyder vi også en række buffeter og selskabsmenuer, som også kan udforskes.
 
           <p style="font-size: 1.2rem;"><strong> Se vores udvalg her </strong></p>
               <a class="button" href="pages/selskaber/buffet.php">Buffet</a>
@@ -56,19 +51,13 @@
               <p> <strong>Telefon:</strong> 23603949 </p>
               <p> <strong>Mail:</strong> Cafefrede@live.dk </p>
         </div>
-        </div>
-
 
       </div>
     </div>
-
-
+</div>
 
 <!--Indrag footer fra filen includes/footer.php-->
 <?php include '../includes/footer.php'; ?>
 
-
-
 </body>
-
 </html>
