74b58ca09f68aede2c9adfd66dc40db948a8538b
Mona Lisa <simoneliasen@gmail.comgit>
2019-05-21 21:05:12 +0200

    Opsat til korius database (include sider) + config.php

--- includes/header.php
+++ includes/header.php
@@ -131,7 +131,7 @@
 
 <?php
 //Opretter forbindelse manelt kontra include, da de ellers er duplikeret i main-content
-$db = mysqli_connect('localhost','root','','admin');
+$db = mysqli_connect('mysql4.unoeuro.com','korius_dk','qbj23ezn','korius_dk_db');
 if(!$db){
   die("Cannot connect to the database" . mysqli_connect_error());
 }
