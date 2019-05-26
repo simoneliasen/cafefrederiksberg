3c545be8e4529b4bdc6716549e1f64a3da750710
Mona Lisa <simoneliasen@gmail.comgit>
2019-05-27 00:05:22 +0200

    Fixede issue ved sletning af items på barmenu

--- admin/pages/php_process/process_barmenu_delete.php
+++ admin/pages/php_process/process_barmenu_delete.php
@@ -1,29 +1,15 @@
-<?php
+<?php 
 require_once '../../config.php';
 
-
   $id = $_GET['id'];
-
-  $query = "SELECT * FROM header WHERE id ='$id'";
-  $results = mysqli_query($db, $query);
-  echo "hello";
-  unlink('../../../header_slide/asd.jpg');
-
-
   $query = "DELETE FROM barmenu WHERE id = '$id'";
   $results = mysqli_query($db, $query);
 
-
   if($results){
-    header("Location: ../bar.php?");
+header("Location: ../bar.php?");
     exit();
   }else {
     die("could not query the database");
   }
-
-
-
-
 mysqli_close($db);
-
 ?>
