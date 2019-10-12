d384cb3a8183cb4ce60f2914e56ad48000307176
simoneliasen <simoneliasen@gmail.com>
2019-10-12 15:18:35 +0200

    added different print on odd/even columns for img

--- pages/selskaber/buffet.php
+++ pages/selskaber/buffet.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Gode Buffeter inklusiv fri øl, vin og vand, vi god mad til gode priser, kom forbi og oplev en af vores interessante buffeter"/>
+  <meta name="description" content="Gode Buffeter inklusiv fri øl, vin og vand, vi god mad til gode priser, kom forbi og oplev en af vores interessante buffeter" />
   <link rel="icon" type="image/png" href="img/favicon.ico">
   <title>Café Frederiksberg</title>
   <link rel="stylesheet" type="text/css" href="../../css/stylesheet.css">
@@ -12,84 +13,90 @@
   <link rel="stylesheet" type="text/css" href="../../css/normalize.css">
   <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700|Poppins&display=swap" rel="stylesheet">
 </head>
+
 <body>
 
   <!-- Mobil navigation -->
-    <div class="container">
+  <div class="container">
     <div class="mobilenav">
       <span onclick="openNav()"><img src="./../../img/hamburger.svg" id="hamburger" width="21px" height="auto"></span>
-        <a href="./index.php">
-          <img src="./../../img/logo.svg" id="fredelogo" alt="Café frederiksberg logo" width="150rem" color="white" height="auto">
-        </a>
+      <a href="./index.php">
+        <img src="./../../img/logo.svg" id="fredelogo" alt="Café frederiksberg logo" width="150rem" color="white" height="auto">
+      </a>
     </div>
-    </div>
+  </div>
 
-    <?php // Tilføjer mulighed for announcementbar
-    include '../../includes/announcement.php'; ?>
+  <?php // Tilføjer mulighed for announcementbar
+  include '../../includes/announcement.php'; ?>
 
   <!--Sidebar mobil  -->
-    <div id="mySidenav" class="sidenav">
-      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-      <a href="./index.php">Forside</a>
-      <a href="./pages/restaurant.php">Menukort</a>
-      <a href="./pages/bar.php">Barkort</a>
-      <a href="./pages/events.php">Events</a>
-      <a href="./pages/selskaber.php">Selskaber<i class="arrowdown" width="2px"></i></a>
-      <a href="./pages/selskaber/buffet.php" class="active" id="dropdown">Buffet</a>
-      <a href="./pages/selskaber/arrangementer.php" id="dropdown">Arrangementer</a>
-      <a href="./pages/selskaber/selskabsmenu.php" id="dropdown">Selskabsmenu</a>
-      <a href="./pages/tidslinje.php">Om os<i class="arrowdown" width="2px"></i></a>
-      <a href="./pages/kontakt.php" id="dropdown">Kontakt</a>
-    </div>
+  <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="./index.php">Forside</a>
+    <a href="./pages/restaurant.php">Menukort</a>
+    <a href="./pages/bar.php">Barkort</a>
+    <a href="./pages/events.php">Events</a>
+    <a href="./pages/selskaber.php">Selskaber<i class="arrowdown" width="2px"></i></a>
+    <a href="./pages/selskaber/buffet.php" class="active" id="dropdown">Buffet</a>
+    <a href="./pages/selskaber/arrangementer.php" id="dropdown">Arrangementer</a>
+    <a href="./pages/selskaber/selskabsmenu.php" id="dropdown">Selskabsmenu</a>
+    <a href="./pages/tidslinje.php">Om os<i class="arrowdown" width="2px"></i></a>
+    <a href="./pages/kontakt.php" id="dropdown">Kontakt</a>
+  </div>
   <script>
-  // viser sidebar ved klik på hamburgermenu
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-  }
+    // viser sidebar ved klik på hamburgermenu
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0px";
+    }
   </script>
 
   <!--Desktop navigation  -->
-    <div class="navigationbar">
-      <nav class="container">
+  <div class="navigationbar">
+    <nav class="container">
       <div class="logo">
         <a href="./index.php"><img src="./../../img/logo.svg" id="logo" alt="Café frederiksberg logo" width="150rem" height="auto"></a>
       </div>
-        <div class="navigationmenu">
+      <div class="navigationmenu">
         <a href="./pages/restaurant.php">Menukort</a>
         <a href="./pages/bar.php">Barkort</a>
         <a href="./pages/events.php">Events</a>
-          <div class="dropdown">
-            <a href="./pages/selskaber.php">Selskaber<i class="arrowdown"></i></a>
-            <div class="dropdown-content">
+        <div class="dropdown">
+          <a href="./pages/selskaber.php">Selskaber<i class="arrowdown"></i></a>
+          <div class="dropdown-content">
             <a href="./pages/selskaber/buffet.php" class="active">Buffet</a>
             <a href="./pages/selskaber/arrangementer.php">Arrangementer</a>
             <a href="./pages/selskaber/selskabsmenu.php">Selskabsmenu</a>
-            </div>
-          </div>
-          <div class="dropdown">
-            <a href="./pages/tidslinje.php">Om os<i class="arrowdown"></i></a>
-            <div class="dropdown-content">
-            <a href="./pages/kontakt.php">Kontakt</a>
-            </div>
           </div>
         </div>
-      </nav>
-    </div>
-    <script>
+        <div class="dropdown">
+          <a href="./pages/tidslinje.php">Om os<i class="arrowdown"></i></a>
+          <div class="dropdown-content">
+            <a href="./pages/kontakt.php">Kontakt</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <script>
     // Tilføjer sort baggrund onscroll
     var $nav = $('.navigationbar');
     $(document).scroll(function() {
-        $nav.css({background: $(this).scrollTop() > 20? "rgba(0, 0, 0, 0.4)":"transparent"});
+      $nav.css({
+        background: $(this).scrollTop() > 20 ? "rgba(0, 0, 0, 0.4)" : "transparent"
+      });
     });
     // Tilføjer sort baggrund til dropdown items
     var $dropdown = $('.dropdown-content');
     $(document).scroll(function() {
-        $dropdown.css({background: $(this).scrollTop() > 20? "rgba(0, 0, 0, 0.4)":"transparent"});
+      $dropdown.css({
+        background: $(this).scrollTop() > 20 ? "rgba(0, 0, 0, 0.4)" : "transparent"
+      });
     });
-    </script>
+  </script>
 
   <?php
   // Indsætter video/billede slider
@@ -98,129 +105,132 @@
   include '../../admin/config.php';
   ?>
 
-<div class="wrapper buffet-menu">
-  <!--Indhold centreret i wrapper-->
-  <div class="container buffet_info_menu">
-    <div class="buffet_infooverskrift">
-      <h2>Café Frederiksberg har et udvalg af lækre buffeter</h2>
-    </div>
-    <div class="row buffet_infotekst">
-      <div class="six columns">
-        <p>Når du holder fest, kan det godt løbe løbsk inden regningen kommer - bare ikke hos os!</p>
-        <p>Vi hylder nemlig princippet om, at man kender udgifterne på forhånd. Vi serverer selvfølgelig gerne drinks og spiritus, men kun efter forudgående aftale, så du selv ved hvad du har sagt ja til. Uanset hvor mange øl Onkel Hans kan drikke, bliver regningen den samme.</p>
+  <div class="wrapper buffet-menu">
+    <!--Indhold centreret i wrapper-->
+    <div class="container buffet_info_menu">
+      <div class="buffet_infooverskrift">
+        <h2>Café Frederiksberg har et udvalg af lækre buffeter</h2>
       </div>
-      <div class="six columns">
-        <p>Vores selskabslokaler er hele tiden booket op og det skyldes ikke mindst, at kunderne kender prisen på forhånd, og at den samtidig er overkommelig. Vi har byens bedste og mest konkurrencedygtige priser på en færdigpakket fest, og det er uanset om Tante Oda holder 70 års.</p>
-      </div>
-    </div>
-    <div class="row buffet_allegener_smiley">
-      <P style="margin-bottom: 0.2rem;">Vi er stolte over at kunne fremvise 5 elite smileys <a href="https://www.findsmiley.dk/25727" target="_blank"><img src="img/kontrolrapport.JPEG" width="100" border="0"></a></p>
-      <p id="allergy">Allegiker/vegetar menu kan bestilles mod tillæg i prisen</p>
-    </div>
-  </div>
-  <hr>
-  <div class="container">
-    <div class="row">
-      <div class="twelve columns">
-        <div class="menulinje-buffet">
-          <ul>
-            <li><a href="pages/selskaber/buffet.php#Buffet_1">Buffet 1</a></li>
-            <li><a href="pages/selskaber/buffet.php#Buffet_2">Buffet 2</a></li>
-            <li><a href="pages/selskaber/buffet.php#Buffet_3">Buffet 3</a></li>
-            <li><a href="pages/selskaber/buffet.php#Buffet_4">Buffet 4</a></li>
-            <li><a href="pages/selskaber/buffet.php#USA_buffet">USA buffet</a></li>
-            <li><a href="pages/selskaber/buffet.php#Italiensk_buffet">Italiensk Buffet</a></li>
-            <li><a href="pages/selskaber/buffet.php#Øko_buffet">Øko buffet</a></li>
-            <li><a href="pages/selskaber/buffet.php#BuffetJul">Jule Buffet</a></li>
-          </ul>
+      <div class="row buffet_infotekst">
+        <div class="six columns">
+          <p>Når du holder fest, kan det godt løbe løbsk inden regningen kommer - bare ikke hos os!</p>
+          <p>Vi hylder nemlig princippet om, at man kender udgifterne på forhånd. Vi serverer selvfølgelig gerne drinks og spiritus, men kun efter forudgående aftale, så du selv ved hvad du har sagt ja til. Uanset hvor mange øl Onkel Hans kan drikke, bliver regningen den samme.</p>
+        </div>
+        <div class="six columns">
+          <p>Vores selskabslokaler er hele tiden booket op og det skyldes ikke mindst, at kunderne kender prisen på forhånd, og at den samtidig er overkommelig. Vi har byens bedste og mest konkurrencedygtige priser på en færdigpakket fest, og det er uanset om Tante Oda holder 70 års.</p>
         </div>
       </div>
+      <div class="row buffet_allegener_smiley">
+        <P style="margin-bottom: 0.2rem;">Vi er stolte over at kunne fremvise 5 elite smileys <a href="https://www.findsmiley.dk/25727" target="_blank"><img src="img/kontrolrapport.JPEG" width="100" border="0"></a></p>
+        <p id="allergy">Allegiker/vegetar menu kan bestilles mod tillæg i prisen</p>
+      </div>
     </div>
-  </div>
-
-
-<!-- Buffet -->
-
-<?php
-
-  $query = "SELECT MAX(buffetNumber) FROM buffet;";
-  $buffetMax = mysqli_fetch_row(mysqli_query($db,$query));
-
-  if(!$results){
-    die("could not query the database" .mysqli_error());
-  }
-
-  $buffetCounter = 1;
-  function buffetPicker() {
-    global $buffetCounter;
-    global $db;
-    $query = "SELECT * FROM buffet WHERE buffetNumber = $buffetCounter;";
-    $results = mysqli_query($db,$query);
-
-    return $results;
-  }
-  function buffetOverskrift() {
-    global $buffetCounter;
-    global $db;
-    $query = "SELECT buffetName FROM buffet WHERE buffetNumber = $buffetCounter LIMIT 1;";
-    $results = mysqli_query($db,$query);
-
-    $overskrift = $results->fetch_assoc();
-    return$overskrift['buffetName'];
-  }
-
-?>
-<?php
-while ($buffetCounter <= $buffetMax[0]):
-	if($buffetCounter %2 == 0):
-		?>
-      <div class="container">
-        <div class="row buffet_even">
-          <div class="six columns">
-            <h2><?=buffetOverskrift()?></h2>
+    <hr>
+    <div class="container">
+      <div class="row">
+        <div class="twelve columns">
+          <div class="menulinje-buffet">
             <ul>
-            <?php
-            $row_result = buffetPicker();
-        		while($row = mysqli_fetch_row($row_result)):
-              ?>
-        			<li class="buffet_list"><?=$row[3]?></li>
-              <?php
-        		endwhile;
-            ?>
+              <li><a href="pages/selskaber/buffet.php#Buffet_1">Buffet 1</a></li>
+              <li><a href="pages/selskaber/buffet.php#Buffet_2">Buffet 2</a></li>
+              <li><a href="pages/selskaber/buffet.php#Buffet_3">Buffet 3</a></li>
+              <li><a href="pages/selskaber/buffet.php#Buffet_4">Buffet 4</a></li>
+              <li><a href="pages/selskaber/buffet.php#USA_buffet">USA buffet</a></li>
+              <li><a href="pages/selskaber/buffet.php#Italiensk_buffet">Italiensk Buffet</a></li>
+              <li><a href="pages/selskaber/buffet.php#Øko_buffet">Øko buffet</a></li>
+              <li><a href="pages/selskaber/buffet.php#BuffetJul">Jule Buffet</a></li>
             </ul>
           </div>
         </div>
-     </div>
+      </div>
+    </div>
+
+
+    <!-- Buffet -->
     <?php
-  	else:
-    ?>
-    <div class="bg_dark">
-      <div class="container">
-        <div class="row buffet_odd">
-          <div class="six columns">
-          <h2><?=buffetOverskrift()?></h2>
-          <ul>
-          <?php
-          $row_result = buffetPicker();
-      		while($row = mysqli_fetch_row($row_result)):
-            ?>
-      			<li class="buffet_list"><?=$row[3]?></li>
-            <?php
-      		endwhile;
-          ?>
-          </ul>
+
+    // return amount of buffetnumbers
+    $query = "SELECT MAX(buffetNumber) FROM buffet;";
+    $buffetMax = mysqli_fetch_row(mysqli_query($db, $query));
+    if (!$results) {
+      die("could not query the database" . mysqli_error());
+    }
+
+    // Returns buffetitems that has specified Buffetcounter. (initializes with 1)
+    $buffetCounter = 1;
+    function buffetPicker()
+    {
+      global $buffetCounter;
+      global $db;
+      $query = "SELECT * FROM buffet WHERE buffetNumber = $buffetCounter;";
+      $results = mysqli_query($db, $query);
+      return $results;
+    }
+
+    //Get BuffetOverskrift related to buffetCounter
+    function buffetOverskrift()
+    {
+      global $buffetCounter;
+      global $db;
+      $query = "SELECT buffetName FROM buffet WHERE buffetNumber = $buffetCounter LIMIT 1;";
+      $results = mysqli_query($db, $query);
+      $overskrift = $results->fetch_assoc();
+      return $overskrift['buffetName'];
+    }
+
+    //While loop that prints items and imgs corresponding to buffetnumber, 
+    //changing between odd/even column if it has even or uneven buffetnumber
+    //Nested while loop prints menuitems
+    while ($buffetCounter <= $buffetMax[0]) :
+      if ($buffetCounter % 2 == 0) : ?>
+        <div class="container">
+          <div class="row buffet_even">
+            <div class="six columns">
+              <h2><?= buffetOverskrift() ?></h2>
+              <ul>
+                <?php
+                    $row_result = buffetPicker();
+                    while ($row = mysqli_fetch_row($row_result)) :
+                      ?>
+                  <li class="buffet_list"><?= $row[3] ?></li>
+                <?php endwhile; ?>
+              </ul>
+            </div>
+            <div class="six columns"> <img src="img/Buffet2.jpg"> </div>
           </div>
+        </div>
+  </div>
+
+  <!-- If BuffetCounter is an odd number echo elements on odd column -->
+<?php else : ?>
+  <div class="bg_dark">
+    <div class="container">
+      <div class="row buffet_odd">
+        <div class="six columns"> <img src="img/Buffet2.jpg" style="float:left;"> </div>
+        <div class="six columns">
+          <h2><?= buffetOverskrift() ?></h2>
+          <ul>
+            <?php
+                $row_result = buffetPicker();
+                while ($row = mysqli_fetch_row($row_result)) :
+                  ?>
+              <li class="buffet_list"><?= $row[3] ?></li>
+            <?php endwhile; ?>
+          </ul>
         </div>
       </div>
     </div>
-  <?php
+  </div>
+
+<?php
   endif;
-  $buffetCounter ++;
+  $buffetCounter++;
 endwhile;
 ?>
 
 <?php include '../../includes/footer.php'; ?>
-<?php mysqli_close($db);?>
+<?php mysqli_close($db); ?>
 
 </body>
+
 </html>
